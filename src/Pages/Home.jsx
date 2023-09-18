8702073c056afe9c372595fa66164161c2c2688e
RahmounFares2001 <faresrahm40@gmail.com>
2023-09-18 13:43:02 +0200

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,23 @@
+import Hero from "../components/Hero";
+import BookCar from "../components/BookCar";
+import PlanTrip from "../components/PlanTrip";
+import PickCar from "../components/PickCar";
+import ChooseUs from "../components/ChooseUs";
+import Testimonials from "../components/Testimonials";
+import Faq from "../components/Faq";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <BookCar />
+      <PlanTrip />
+      <PickCar />
+      <ChooseUs />
+      <Testimonials />
+      <Faq />
+    </>
+  );
+}
+
+export default Home;
